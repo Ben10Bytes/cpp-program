c8a56e4959c67e6ee96a9ef73efe8966bccba984
Ben10Byte <rk4096172@gmail.com>
2025-07-03 17:43:15 +0530

    cpp

---
+++ cin.cpp/cin.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //cout << "Hello" << endl;
+    //cout << "The value of n is : " << n << endl;
+
+    if (n>0){
+        cout << "N is positive " << endl;
+    }
+
+    else{
+        cout << "N is negative " << endl;
+    }
+
+
+}
---
+++ intro.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*
+    cout<<"Pranipat Jahaan\n";
+    int a = 21;
+    cout<< a << endl;
+
+    char b = '*';
+    cout << b << endl;
+
+    float c = 1.2;
+    cout << c << endl;
+    
+    double d = 1.21;
+    cout << d << endl;
+
+    int size = sizeof(a);
+    cout <<"Size of a is : "<<size << endl;
+    */
+   
+   int a = 2;
+   int b = 3;
+   
+   bool first = (a==b);
+   cout << first << endl;
+
+   bool second = (a>=b);
+   cout << second << endl;
+
+   bool third = (a<=b);
+   cout << third << endl;
+
+   bool fourth = (a!=b);
+   cout << fourth << endl;
+
+   int c = 0;
+   cout << !c << endl;
+   
+    return 0;
+}
