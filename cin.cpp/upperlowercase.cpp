faa4e914f7c551387b04f5b3bee726ca813eb444
Ben10Byte <rk4096172@gmail.com>
2025-07-14 23:38:40 +0530

    upercase and lowercase
    How can create a program for upercase and lowercase

---
+++ cin.cpp/upperlowercase.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Give me input :"<<endl;
+    cin>>ch;
+
+    if(ch>='a' && ch<='z'){
+        cout<<"The input is lowercase"<<endl;
+    }
+
+    else if(ch>='A' && ch<='Z'){
+        cout<<"The input is upercase"<<endl;
+    }
+    else if(ch>='0' && ch<='9'){
+        cout<<"The input is numeric value"<<endl;
+    }
+
+}
