a03bb6a40972f36cb8a51d2fe39530d5d4e4845b
Ben10Byte <rk4096172@gmail.com>
2025-07-18 16:29:23 +0530

    check while loop

--- cin.cpp/while_loop/while_loop1.cpp
+++ cin.cpp/while_loop/while_loop1.cpp
@@ -1,14 +1,2 @@
 //"Write a program in C++ to print numbers from 1 to N using a while loop. Take N as input from the user."
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int i = 1;
-    while (i<=n){
-        cout<< i <<" ";
-        i = i + 1;
-        
-    }
-
-}
+#include<iostream
