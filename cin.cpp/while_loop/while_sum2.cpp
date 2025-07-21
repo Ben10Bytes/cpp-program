cadeaf578667fb976261e05e4c22bf436fecfd9d
Ben10Byte <rk4096172@gmail.com>
2025-07-21 23:43:00 +0530

    The sum of 1 to n number

---
+++ cin.cpp/while_loop/while_sum2.cpp
@@ -0,0 +1,17 @@
+// The sum of 1 to n number
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int i = 1;
+    int sum = 0;
+
+    while(i<=n){
+        sum = sum + i;
+        i = i + 1;
+    }
+    cout<<"The sum of number is : "<<sum <<endl;
+}
