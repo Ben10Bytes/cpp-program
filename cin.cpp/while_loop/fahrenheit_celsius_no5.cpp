c1168b7740234e9a7454aa986c7112520ca7abcb
Ben10Byte <rk4096172@gmail.com>
2025-07-24 00:43:33 +0530

    how to convert farhenheit to celsius

---
+++ cin.cpp/while_loop/fahrenheit_celsius_no5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float f;
+    cin>>f;
+    float c = (f - 32) * 5/9;
+    cout << f << " °F is : "<< c << " °C" << endl; // This line not finished for the degree symbol
+    return 0;
+
+}
