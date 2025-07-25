5510bdde94eb192067b1e737ede5e437efe4e421
Ben10Byte <rk4096172@gmail.com>
2025-07-26 02:34:59 +0530

    create a equal number of rows and columns with same number using while loop

---
+++ cin.cpp/while_loop/patern_whieloop.cpp
@@ -0,0 +1,19 @@
+// create a equal number of rows and columns with same number using while loop
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i <=n){
+        int j = 1;
+        while(j <=n){
+            cout << i;
+            j++;
+            
+        }
+        cout << endl; // Move to the next line after printing one row
+        i++; // Move to the next row
+    }
+}
