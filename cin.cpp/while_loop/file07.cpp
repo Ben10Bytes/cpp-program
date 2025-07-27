64982d720813dc944a6595408117d581889a7f82
Ben10Byte <rk4096172@gmail.com>
2025-07-28 02:49:36 +0530

    This program checks if a number is prime or not using a while loop

--- cin.cpp/while_loop/file07.cpp
+++ cin.cpp/while_loop/file07.cpp
@@ -1,4 +1,3 @@
-// prime_or_not.cpp
 // This program checks if a number is prime or not using a while loop.
 
 #include<iostream>
@@ -7,9 +6,16 @@ int main(){
     int n;
     cin >> n;
     int i = 2;
+    // Start checking from the first prime number, which is 2
+    // A prime number is greater than 1 and has no divisors other than 1
+
+    if(n <= 1){
+        cout << "Not Prime" << endl;
+        return 0;
+    }
     
-    while(i<n){
-        if(n % i ==0){
+    while(i<n){  // while(i <= n) is toatally wrong, we need to check divisors less than n
+        if(n % i ==0){ 
             cout << "Not Prime" << endl;
             return 0; // Exit the program if not prime  
 
