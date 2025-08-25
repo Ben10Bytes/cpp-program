0db132dd5a12c58a12ed18e17dd2c7281d8782ef
Ben10Byte <rk4096172@gmail.com>
2025-08-25 16:14:23 +0530

    practice

--- cin.cpp/while_loop/practie.cpp
+++ cin.cpp/while_loop/practie.cpp
@@ -3,15 +3,12 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    
-    int i = 2;
-    int sum = 0;
-    
+
+    int i = 1;
     while(i<=n){
-        if(i%2==0){
-            sum = sum + i;
+        if(i%2!=0){
+            cout<<i<<" ";
         }
         i++;
     }
-    cout<<sum<<endl;
-}
+} 
