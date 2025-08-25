b54ec41f232220e2b21681d0d8f6f6336d8af97c
Ben10Byte <rk4096172@gmail.com>
2025-08-25 16:24:24 +0530

    practice

--- cin.cpp/while_loop/practie.cpp
+++ cin.cpp/while_loop/practie.cpp
@@ -5,10 +5,13 @@ int main(){
     cin>>n;
 
     int i = 1;
+    int sum = 0;
     while(i<=n){
         if(i%2!=0){
-            cout<<i<<" ";
+            sum = sum + i;
+            
         }
         i++;
     }
+    cout<<sum<<endl;
 } 
