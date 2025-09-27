18344d89271bc9bff8563bd93ee00916eaeb94fd
Ben10Byte <rk4096172@gmail.com>
2025-09-27 16:28:44 +0530

    update

--- cin.cpp/while_loop/practie.cpp
+++ cin.cpp/while_loop/practie.cpp
@@ -3,15 +3,9 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-
-    int i = 2;
-    while(i<n){
-        if(n%i==0){
-            cout<<"Not prime for"<<i<<endl;    
-        }
-        else{
-            cout<<"Prime for"<<i<<endl;
-        }
-         i++;       
+    int i = 1;
+    while(i<=n){
+        cout<<i<<;
+        i++;
     }
 }
