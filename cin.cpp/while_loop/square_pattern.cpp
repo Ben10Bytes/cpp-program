bfb6de5108cadf7ed1a0dc31da8b4b8671f83d0c
Ben10Byte <rk4096172@gmail.com>
2026-05-07 10:46:25 +0530

    pattern

--- cin.cpp/while_loop/square_pattern.cpp
+++ cin.cpp/while_loop/square_pattern.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n = 4;
+    int n = 5;
     for ( int i =0; i <n; i++) {
         for ( int j = 0; j < n; j++) {
             if (i==0 || i==n-1 || j==0 || j==n-1){
