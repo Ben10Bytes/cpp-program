c2771ec107221eeed3371c8d87db4f9d89b2dda2
Ben10Byte <rk4096172@gmail.com>
2026-05-11 09:06:53 +0530

    sum and maximum number

--- cin.cpp/while_loop/cls_tr_pr.cpp
+++ cin.cpp/while_loop/cls_tr_pr.cpp
@@ -6,31 +6,73 @@
 
 //     string z = x + y;
 
-#include <iostream>
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// void printArray(int arr[], int size, string message) {
+//     cout << message << endl;
+
+//     for(int i = 0; i < size; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     cout << endl;
+// }
+
+// void question1PrintAll() {
+//     int arr[] = {5, 10, 15, 20, 25};
+//     int size = 5;
+
+//     cout << "=== print all elements ===" << endl;
+
+//     printArray(arr, size, "Array Elements:");
+// }
+
+// int main() {
+
+//     question1PrintAll();
+
+//     return 0;
+// }
+
+
+
+
+
+// Sum of the array element using loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[] = {10,20,30,40};
+//     int sum = 0;
+
+//     for (int i = 0; i < 4; i++){
+//         sum = sum + arr[i];
+//     }
+
+//     cout<<"Sum = " << sum;
+//     return 0;
+
+// }
+
+
+#include<iostream>
 using namespace std;
+int main(){
+    int arr[] = {10,20,30,40};
+    int max = arr[0];
 
-void printArray(int arr[], int size, string message) {
-    cout << message << endl;
-
-    for(int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+    for (int i = 0; i < 4; i++){
+        if(arr[i] > max);
+        max = arr[i];
     }
 
-    cout << endl;
-}
-
-void question1PrintAll() {
-    int arr[] = {5, 10, 15, 20, 25};
-    int size = 5;
-
-    cout << "=== print all elements ===" << endl;
-
-    printArray(arr, size, "Array Elements:");
-}
-
-int main() {
-
-    question1PrintAll();
-
+    cout<<"maximum number is :  " << max;
     return 0;
+
 }
