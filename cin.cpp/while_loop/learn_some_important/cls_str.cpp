0382a65082a71170ad3af24722dc2d678c6d8d7b
Ben10Byte <rk4096172@gmail.com>
2026-05-12 13:43:51 +0530

    without using namespace std

--- cin.cpp/while_loop/learn_some_important/cls_str.cpp
+++ cin.cpp/while_loop/learn_some_important/cls_str.cpp
@@ -1,12 +1,19 @@
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// int main(){
+//     string fullname;
+//     cout <<"Enter Your Name "<<endl;
+//     getline(cin,fullname);
+//     cout <<"Your Name is : " <<fullname;
+
+//     return 0;
+
+// }
+
 #include<iostream>
 #include<string>
-using namespace std;
 int main(){
-    string fullname;
-    cout <<"Enter Your Name "<<endl;
-    getline(cin,fullname);
-    cout <<"Your Name is : " <<fullname;
-
-    return 0;
-
+    std::string greeting = "Hi";
+    std::cout<< greeting;
 }
