fb538b741f8ea64911fdaf413316021d216de778
Ben10Byte <rk4096172@gmail.com>
2026-05-12 12:55:24 +0530

    String using getline

---
+++ cin.cpp/while_loop/learn_some_important/cls_str.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string fullname;
+    cout <<"Enter Your Name "<<endl;
+    getline(cin,fullname);
+    cout <<"Your Name is : " <<fullname;
+
+    return 0;
+
+}
