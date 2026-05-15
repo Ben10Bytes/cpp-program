70f30a044980624f13403d28b791904f00acb4ff
Ben10Byte <rk4096172@gmail.com>
2026-05-15 08:04:26 +0530

    matrix_thorughwise sum operation

---
+++ Matrix/matrix_throughwise_sum2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    for(int i =0; i<3; i++){
+        int sum = 0;
+        cout << "{";
+
+        for(int j =0; j < 3; j++){
+            cout<< arr[i][j];
+            sum = sum + arr[i][j];
+
+            if(j != 2){
+                cout << ",";
+            }
+        }
+        cout << "} = " << sum << endl;
+    }
+    return 0;
+}
