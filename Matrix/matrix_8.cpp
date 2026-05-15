341e7816b199d8b559bb465aef22664f9b6eb835
Ben10Byte <rk4096172@gmail.com>
2026-05-15 09:03:57 +0530

    matrix sorting techniques

---
+++ Matrix/matrix_8.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int arr[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    for(int j = 0; j < 3; j++) {
+        cout << arr[0][j] << " ";
+    }
+    for(int i = 1; i < 3; i++) {
+        cout << arr[i][2] << " ";
+    }
+    for(int j = 1; j >= 0; j--) {
+        cout << arr[2][j] << " ";
+    }
+    for(int i = 1; i >= 1; i--) {
+        cout << arr[i][0] << " ";
+    }
+    
+    return 0;
+}
