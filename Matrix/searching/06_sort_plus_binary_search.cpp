48c0a34d397522bcea066e11e1dc57cd97355f2c
Ben10Byte <rk4096172@gmail.com>
2026-05-18 12:01:08 +0530

    sort plus binary

---
+++ Matrix/searching/06_sort_plus_binary_search.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[] = { 40 , 10 , 30, 20, 50};
+    int n = 5;
+
+    int key = 30;
+
+    sort(arr, arr+n){
+        
+    }
+
+}
