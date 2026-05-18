0fded13c3b9ffd28fe1792eea8b515ccfe1e64fa
Ben10Byte <rk4096172@gmail.com>
2026-05-18 09:13:45 +0530

    sort and searching

---
+++ Matrix/searching/02_binary.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = { 10, 20, 30, 40, 50 };
+
+    int n = 5;
+    int key = 40;
+
+    int start = 0;
+    int end = n - 1;
+
+    while(start <= end){
+        int mid = (start + end) / 2;
+        if(arr[mid]==key){
+            cout << "Element Found at index " << " " << mid;
+            return 0;
+        }
+        else if(key > arr[mid]){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    cout << "Element not found";
+    return 0;
+}
