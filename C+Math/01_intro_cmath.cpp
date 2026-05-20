d27a1d57549a93e52b4632296ecfdb572a703a59
Ben10Byte <rk4096172@gmail.com>
2026-05-20 09:27:02 +0530

    update in c math

--- C+Math/01_intro_cmath.cpp
+++ C+Math/01_intro_cmath.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main(){
     cout<<"Square root of 6 is : " <<sqrt(6)<<endl;
     cout<<"The nearest value of 2.6 is : " <<round(2.6)<<endl;
-    cout<<"The value of log 2 is : " <<log(2);
+    cout<<"The value of log 2 is : " <<log(2)<<endl;
+    cout<<max(5,6)<<endl;
+    cout<<min(5,6)<<endl;
 }
