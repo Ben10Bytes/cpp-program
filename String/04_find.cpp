f8f18426e1c656844c3381a0a8965adbb6fa2db3
Ben10Byte <rk4096172@gmail.com>
2026-05-20 11:31:58 +0530

    find string using and how this work

---
+++ String/04_find.cpp
@@ -0,0 +1,20 @@
+/*
+ String Searching funciton 
+ Because they search for:
+    1)character
+    2)word
+    3)substring
+
+
+    note : In java find is known as indexof
+*/
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    string s1 = " c++ language ";
+    cout <<s1.find("lang");
+    
+    return 0;
+}
