f778edd53868c3d15e5e1f8d6e9f0d47c337b5ed
Ben10Byte <rk4096172@gmail.com>
2026-05-20 09:52:48 +0530

    string function learn this is length

---
+++ String/01_length.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    string s1 = "Parul university";
+    string s2 = "C++ Programming";
+
+    cout<<s1<<endl;
+    cout<<s2<<endl;
+    
+    return 0;
+}
