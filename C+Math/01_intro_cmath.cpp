8905ed12b7fcdb577655e8867b60124b6e7d2aad
Ben10Byte <rk4096172@gmail.com>
2026-05-20 09:14:29 +0530

    intro c mathc

---
+++ C+Math/01_intro_cmath.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    cout<<"Square root of 6 is : " <<sqrt(6)<<endl;
+    cout<<"The nearest value of 2.6 is : " <<round(2.6)<<endl;
+    cout<<"The value of log 2 is : " <<log(2);
+}
