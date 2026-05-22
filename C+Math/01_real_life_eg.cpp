823ada5e8517869a66a9a196116b4cccbfa809ff
Ben10Byte <rk4096172@gmail.com>
2026-05-22 11:29:18 +0530

    implementation on this code

--- C+Math/01_real_life_eg.cpp
+++ C+Math/01_real_life_eg.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
+    // int myAge;
+    // int votingAge = 18;
+
+    // cout<<"Enter the Age : ";
+    // cin>>myAge;
+    // cout<<"Eligible for voting "<<(myAge>=votingAge);
+
+
     int myAge;
     int votingAge = 18;
-
-    cout<<"Enter the Age : ";
+    
+    cout<<"Enter your age : ";
     cin>>myAge;
-    cout<<"Eligible for voting "<<(myAge>=votingAge);
+
+    if(myAge>=votingAge){
+        cout<<"Eligible for voting"<<endl;
+    } else{
+        cout<<"Not Eligible for voting";
+    }
 }
