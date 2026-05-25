580bfc7ff5489ffb168c8ff9fe8d19938d4f54ce
Ben10Byte <rk4096172@gmail.com>
2026-05-25 12:19:57 +0530

    practice

---
+++ OOP's/08_practice.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+};
+
+int main() {
+    Calculator calc;
+
+    cout << calc.add(5, 10) << endl;
+    cout << calc.add(5.5, 11.5) << endl;
+    cout << calc.add(5, 10, 5) << endl;
+
+    return 0;
+}
