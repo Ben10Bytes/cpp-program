80e94accab01fe843dc35e8ba15e2feb2c5bf6a7
Ben10Byte <rk4096172@gmail.com>
2026-05-25 11:47:56 +0530

    if else statement use

--- OOP's/05_multiple_concept_use.cpp
+++ OOP's/05_multiple_concept_use.cpp
@@ -7,20 +7,19 @@ private:
     int rollNumber;
 
 public:
-    // 1. CONSTRUCTOR (Runs automatically when object is created)
+ 
     Student() {
         std::cout << "Constructor called: Creating a new student record..." << std::endl;
         name = "Unknown";
         rollNumber = 0;
     }
 
-    // 2. METHOD 1: Setter (Uses 'this' keyword to avoid name confusion)
+
     void setDetails(std::string name, int rollNumber) {
         this->name = name;
         this->rollNumber = rollNumber;
     }
 
-    // 3. METHOD 2: Display Function
     void displayInfo() {
         std::cout << "Student Name: " << name << std::endl;
         std::cout << "Roll Number: " << rollNumber << std::endl;
@@ -28,10 +27,8 @@ public:
 };
 
 int main() {
-    // 4. OBJECT (Creates an instance of the Student class)
-    Student s; 
 
-    // Calling the methods using the object
+    Student s; 
     s.setDetails("Rohit", 101);
     s.displayInfo();
 
---
+++ if_else/01_if_else.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x = 18;
+    if( x < 20) {
+        cout << "Eligible";
+    } else {
+        cout << "Not Eligible";
+    }
+}
