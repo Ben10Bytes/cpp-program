19672d3f73a9a1cc26260b20d4a2051a763ce3ad
Ben10Byte <rk4096172@gmail.com>
2026-05-27 12:09:47 +0530

    overloading difference sequence

--- somequestion/02_overloading_diff_sequ.cpp
+++ somequestion/02_overloading_diff_sequ.cpp
@@ -1,2 +1,25 @@
 #include<iostream>
-#include<ios
+#include<string>
+using namespace std;
+
+class Display {
+public:
+    void show(int a, string b){
+        cout << "Output int string (" << a << ", " << b << ");" << endl;
+    }
+
+    void show(string a, int b){
+        cout << "Output int string (" << a << ", " << b << ");" << endl;
+    }
+
+
+    
+};
+
+int main() {
+    Display d;
+
+    d.show(10, "Hello");
+    d.show("World", 20);
+    return 0;
+}
