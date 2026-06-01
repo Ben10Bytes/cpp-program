a3f4522b4bee085a3e82b24289319b0d64360b59
Ben10Byte <rk4096172@gmail.com>
2026-06-01 08:24:35 +0530

    Recursion

---
+++ Recursion/01_Recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printhello(int n){
+    if(n == 0){
+        return;
+
+        //cout << "Hello " << endl;
+    }
+    cout <<"else" <<endl;
+}
+int main(){
+    int n;
+    cin >> n;
+    printhello(n);
+}
+
+
---
+++ Recursion/02_Recursive_Countdown.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void printnumber(int n) {
+    if (n == 0) {
+        return;
+    }
+    cout << n << " ";
+    printnumber(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter number: " << endl;
+    cin >> n;
+
+    printnumber(n);
+
+    return 0;
+}
