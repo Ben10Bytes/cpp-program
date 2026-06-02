c49a819319b326ea4c792efd0b29ec580b094bb2
Ben10Byte <rk4096172@gmail.com>
2026-06-02 08:31:37 +0530

    permuation method we use

---
+++ Recursion/10_Permutation.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void backtrack(vector<int>& nums, int start, vector<vector<int>>& result){
+    if (start == nums.size()) {
+        result.push_back(nums);
+        return;
+    }
+    for (int i = start; i < nums.size(); i++) {
+        swap(nums[start], nums[i]);
+        backtrack(nums, start + 1, result);
+        swap(nums[start], nums[i]);
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> result;
+    backtrack(nums, 0, result);
+
+    for(const auto& perm : result) {
+        for(int x : perm) cout << x << " ";
+        cout << endl;
+    }
+    return 0;
+}
