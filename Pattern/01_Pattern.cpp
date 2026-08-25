eb7c8e90874b58b057af87d4ce31cbdbbc6f44ed
Ben10Byte <rk4096172@gmail.com>
2026-08-26 01:31:02 +0530

    Same number of row  & column pattern style

--- Pattern/01_Pattern.cpp
+++ Pattern/01_Pattern.cpp
@@ -12,6 +12,13 @@ int main(){
     int i = 1;
     
     while(i <= n){
-        
+
+        int j = 1;
+        while(j <= n){
+            cout << "* ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
     }
 }
