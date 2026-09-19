ef61c94f351c532af47008d315a74f86ab6e03dc
Ben10Byte <rk4096172@gmail.com>
2026-09-20 00:27:10 +0530

    I learn new pattern

---
+++ Pattern/15_Pattern.cpp
@@ -0,0 +1,23 @@
+// A  
+// B  B  
+// C  C  C  
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        char ch = 'A' + i - j;
+        while(j <= i){
+            cout << ch << "  ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    return 0;
+}
