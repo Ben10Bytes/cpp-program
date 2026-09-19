8b4a5a2a4adb36b71501d6a93d78e2591a491929
Ben10Byte <rk4096172@gmail.com>
2026-09-19 23:32:08 +0530

    practice pattern in practice folder

--- Practice/practice.cpp
+++ Practice/practice.cpp
@@ -264,6 +264,35 @@
 
 
 
+// A B C D 
+// B C D E 
+// C D E F 
+// G H I J 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        while(j <= n){
+            char ch = 'A' + i + j - 2;
+            cout << ch << "  ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    return 0;
+}
+    
+
+
+
+
 
 
 
