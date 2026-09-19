a5b5cf36f83fbecc6d517222059ce4c11dbca597
Ben10Byte <rk4096172@gmail.com>
2026-09-19 18:21:51 +0530

    practice pattern in practice folder

--- Practice/practice.cpp
+++ Practice/practice.cpp
@@ -104,6 +104,28 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+//     int count = 1;
+//     while(i <= n){
+//         int j = 1;
+//         while(j <= i){
+//             cout <<count;
+//             count = count + 1;
+//             j = j + 1;
+//         }
+//         cout<<endl;
+//         i = i + 1;
+//     }
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -111,13 +133,13 @@ int main(){
     cin >> n;
 
     int i = 1;
-    int count = 1;
     while(i <= n){
         int j = 1;
+        int value = i;
         while(j <= i){
-            cout <<count;
-            count = count + 1;
-            j = j + 1;
+            cout<< value;
+            value = value + 1;
+            j = j +1;
         }
         cout<<endl;
         i = i + 1;
